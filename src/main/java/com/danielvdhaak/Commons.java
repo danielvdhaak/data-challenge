76571947c61be7d21f071f8474b24d3cc4afef5a
danielvdhaak <danielvdhaak@gmail.com>
2021-05-09 14:03:30 +0200

    Corrected Scala dependencies

---
+++ src/main/java/com/danielvdhaak/Commons.java
@@ -0,0 +1,14 @@
+package com.danielvdhaak;
+
+/**
+ * Loads string configurations from environment variables. 
+ * Assumes default values if no environment variables are defined.
+ */
+public class Commons {
+    public final static String APP_KAFKA_TOPIC = System.getenv("APP_KAFKA_TOPIC") != null ?
+        System.getenv("APP_KAFKA_TOPIC") : "words";
+    public final static String APP_KAFKA_SERVER = System.getenv("APP_KAFKA_SERVER") != null ?
+        System.getenv("APP_KAFKA_SERVER") : "localhost:9092";
+    public final static String APP_ZOOKEEPER_SERVER = System.getenv("APP_ZOOKEEPER_SERVER") != null ?
+        System.getenv("APP_ZOOKEEPER_SERVER") : "localhost:32181";
+}
