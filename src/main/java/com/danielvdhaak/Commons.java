32987db21eaf2f7b2520f52c06b8f5e7f966b75e
Daniel van den Haak <danielvdhaak@gmail.com>
2022-11-08 13:53:19 +0100

    Changed topic name

--- src/main/java/com/danielvdhaak/Commons.java
+++ src/main/java/com/danielvdhaak/Commons.java
@@ -6,9 +6,9 @@ package com.danielvdhaak;
  */
 public class Commons {
     public final static String APP_KAFKA_TOPIC = System.getenv("APP_KAFKA_TOPIC") != null ?
-        System.getenv("APP_KAFKA_TOPIC") : "netflix";
+        System.getenv("APP_KAFKA_TOPIC") : "crypto";
     public final static String APP_KAFKA_SERVER = System.getenv("APP_KAFKA_SERVER") != null ?
-        System.getenv("APP_KAFKA_SERVER") : "localhost:9092";
+        System.getenv("APP_KAFKA_SERVER") : "localhost:9094";
     public final static String APP_ZOOKEEPER_SERVER = System.getenv("APP_ZOOKEEPER_SERVER") != null ?
         System.getenv("APP_ZOOKEEPER_SERVER") : "localhost:2181";
 }
