f37cceedfa919f5c3e142b9b8a1742f73f9add44
Daniel van den Haak <danielvdhaak@gmail.com>
2022-11-09 18:05:52 +0100

    Removed redundant tests

--- src/test/java/com/danielvdhaak/HttpUtilsTest.java
+++ src/test/java/com/danielvdhaak/HttpUtilsTest.java
@@ -1,24 +1,14 @@
 package com.danielvdhaak;
 
 import org.junit.Test;
-
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.URIException;
 
 
 public class HttpUtilsTest {
-    
-    @Test
-    public void assertHttpClientType() {
-        Object httpClient = HttpUtils.createHttpClient(0);
-
-        assertThat(httpClient, instanceOf(HttpClient.class));
-    }
 
     @Test
     public void testHttpClientTimeout() {
@@ -33,14 +23,18 @@ public class HttpUtilsTest {
     }
 
     @Test
-    public void assertGetRequestType() {
-        HttpMethod httpMethod = HttpUtils.createGetRequest("null");
+    public void testRequestUrl() throws URIException {
+        String url = "https://api.binance.com/api/v3/ping";
+        HttpMethod request = HttpUtils.createGetRequest(url);
 
-        assertThat(httpMethod, instanceOf(HttpMethod.class));
+        assertEquals(request.getURI().toString(), url);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testBadRequestUrl() {
         HttpMethod httpMethod = HttpUtils.createGetRequest("Not a URL");
     }
+
+
+
 }
