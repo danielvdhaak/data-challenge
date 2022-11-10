079965fc75731eea75c1659bea8508ef6dac7a57
Daniel van den Haak <danielvdhaak@gmail.com>
2022-11-10 15:26:34 +0100

    Parameterized configuration strings

--- src/main/java/com/danielvdhaak/KafkaProducerCrypto.java
+++ src/main/java/com/danielvdhaak/KafkaProducerCrypto.java
@@ -14,25 +14,25 @@ import org.apache.commons.httpclient.HttpMethod;
 public class KafkaProducerCrypto {
     private static final Logger logger = LogManager.getLogger(KafkaProducerCrypto.class);
 
+    private static final int APP_PRODUCER_INTERVAL = System.getenv("APP_PRODUCER_INTERVAL") != null ?
+        Integer.parseInt(System.getenv("APP_PRODUCER_INTERVAL")) : 100;
+    private static final String APP_PRODUCER_API_ENDPOINT = System.getenv("APP_PRODUCER_API_ENDPOINT") != null ?
+        System.getenv("APP_PRODUCER_API_ENDPOINT") : "https://api.binance.com/api/v3/trades?symbol=BTCUSDT&limit=50";
+
     public static void main(final String... args) 
-    {
-        // Declare API endpoint
-        String url = "https://api.binance.com/api/v3/trades?symbol=BTCUSDT&limit=50";
-        
+    {        
         // Create singular HttpClient object
         HttpClient client = HttpUtils.createHttpClient(0);
 
         // Initialize producer class
         final Producer<String, String> producer = createProducer();
-        int EXAMPLE_PRODUCER_INTERVAL = System.getenv("APP_PRODUCER_INTERVAL") != null ?
-                Integer.parseInt(System.getenv("APP_PRODUCER_INTERVAL")) : 100;
 
         try {
             while (true) {
                 long start = System.nanoTime();
 
                 // Send GET request
-                HttpMethod response = HttpUtils.sendGetRequest(client, url);
+                HttpMethod response = HttpUtils.sendGetRequest(client, APP_PRODUCER_API_ENDPOINT);
                 String responseBody = HttpUtils.parseResponseBody(response);
 
                 // Skip on empty response body
@@ -51,8 +51,8 @@ public class KafkaProducerCrypto {
 
                 // Determine time to delay
                 float timeElapsed = ((float)(System.nanoTime() - start))*1e-6f;
-                int delay = EXAMPLE_PRODUCER_INTERVAL - (int)timeElapsed >= 0 ? 
-                    EXAMPLE_PRODUCER_INTERVAL - (int)timeElapsed : 0;
+                int delay = APP_PRODUCER_INTERVAL - (int)timeElapsed >= 0 ? 
+                    APP_PRODUCER_INTERVAL - (int)timeElapsed : 0;
                 
                 Thread.sleep(delay);
             }
@@ -67,7 +67,7 @@ public class KafkaProducerCrypto {
     private static Producer<String, String> createProducer() {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, Commons.APP_KAFKA_SERVER);
-        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaProducerNetflix");
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         return new KafkaProducer<>(props);
