19eaf89d48c745b00eb718d9f20a4299fbb783a7
Daniel van den Haak <danielvdhaak@gmail.com>
2023-01-12 14:27:32 +0100

    Connector docstrings

--- src/main/java/com/danielvdhaak/cassandra/Connector.java
+++ src/main/java/com/danielvdhaak/cassandra/Connector.java
@@ -10,6 +10,7 @@ import com.datastax.driver.core.Host;
 import com.datastax.driver.core.Metadata;
 import com.datastax.driver.core.Cluster.Builder;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.exceptions.NoHostAvailableException;
 
 
 public class Connector {
@@ -19,7 +20,14 @@ public class Connector {
     private Session session;
     private Set<Host> hosts;
 
-    public void connect(String node, Integer port) {
+    /**
+     * Connects to a Cassandra cluster via a provided node and port.
+     * 
+     * @param node the Cassandra node host address to connect to (this can be any accessible node in the cluster)
+     * @param port the port to use to connect to the Cassandra host
+     * @throws NoHostAvailableException if the provided cluster host is not initialized yet or does not exist
+     */
+    public void connect(String node, Integer port) throws NoHostAvailableException {
         Builder builder = Cluster.builder().addContactPoint(node);
         if (port != null) {
             builder.withPort(port);
@@ -38,14 +46,27 @@ public class Connector {
         hosts = metadata.getAllHosts();
     }
 
+    /**
+     * Returns the current session.
+     * 
+     * @return the current session
+     */
     public Session getSession() {
         return this.session;
     }
 
+    /**
+     * Returns all known cluster hosts.
+     * 
+     * @return all known cluster hosts
+     */
     public Set<Host> getHosts() {
         return this.hosts;
     }
 
+    /**
+     * Closes the current session.
+     */
     public void close() {
         session.close();
         cluster.close();
